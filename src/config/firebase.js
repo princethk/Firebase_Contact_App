b28f66f5c2f631a5afa7c6d706e823d98ec4643a
Prince Thakur <90766332+princethk@users.noreply.github.com>
2024-03-25 21:55:08 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,19 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC0XqHQCgtVo9HNWY5nOzyfoJXNzYtYlQg",
-  authDomain: "fir-contact-app-88a0e.firebaseapp.com",
-  projectId: "fir-contact-app-88a0e",
-  storageBucket: "fir-contact-app-88a0e.appspot.com",
-  messagingSenderId: "150592749520",
-  appId: "1:150592749520:web:352136352e7561800103d5"
-};
+
+// unique key
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const db=getFirestore(app);
